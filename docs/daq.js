cf3131ce853c34afc78c6956c10cc0591ec161df
emitterpark <42601356+emitterpark@users.noreply.github.com>
2020-03-21 16:12:07 +0300

    x

--- docs/daq.js
+++ docs/daq.js
@@ -200,7 +200,7 @@
       }                    
       if (lorawanForm.checkValidity()) {
         statusDisp.textContent = 'validaion ok';
-        item = atModemForm.querySelector('#at-command');
+        //item = atModemForm.querySelector('#at-command');
         const encoder = new TextEncoder();
         //const view = encoder.encode(item.value + '\r\n'); 
         const view = encoder.encode('at+version\r\n');       
